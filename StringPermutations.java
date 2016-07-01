ecfe04ca5b86f800e5c61cacd5c68a0746d76697
gunnu <prashanth.pasham15@gmail.com>
2016-07-01 09:35:15 +0530

    first commit

---
+++ StringPermutations.java
@@ -0,0 +1,33 @@
+package preparation;
+
+public class StringPermutations {
+public static void main(String args[])
+{
+	
+	permutation("hello");
+}
+
+
+public static void permutation(String input)
+{ 
+	permutation("", input);
+} 
+ static void permutation(String perm, String word)
+{ 
+	if (word.isEmpty()) 
+	{ 
+		System.out.println(perm + word); 
+		} 
+	else 
+	{ 
+		for (int i = 0; i<word.length(); i++) 
+		{ 
+			
+			
+			
+			permutation(perm + word.charAt(i), word.substring(0, i) + word.substring(i + 1, word.length())); 
+		} 
+		} 
+	} 
+}
+
